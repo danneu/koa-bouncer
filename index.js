d8d6adcbaa21c14de78e569c36af329f73e13e63
Ryan Havar <ryan@moneypot.com>
2015-10-18 23:25:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -397,7 +397,7 @@ exports.middleware = function middleware() {
       return new Validator({
         ctx: self,
         key: key,
-        val: self.vals[key] === undefined ? self.params[key] : self.vals[key],
+        val: val !== undefined ? val : (self.vals[key] !== undefined ? self.vals[key] : self.params[key]),
         vals: self.vals,
         type: 'param'
       });
@@ -406,16 +406,16 @@ exports.middleware = function middleware() {
       return new Validator({
         ctx: self,
         key: key,
-        val: self.vals[key] === undefined ? self.query[key] : self.vals[key],
+        val: val !== undefined ? val : (self.vals[key] == undefined ?  self.vals[key] : self.query[key]),
         vals: self.vals,
         type: 'query'
       });
     };
-    this.validateBody = function(key) {
+    this.validateBody = function(key, val) {
       return new Validator({
         ctx: self,
         key: key,
-        val: self.vals[key] === undefined ? self.request.body[key] : self.vals[key],
+        val: val !== undefined ? val : (self.vals[key] !== undefined ? self.vals[key] : self.request.body[key]),
         vals: self.vals,
         type: 'body'
       });
